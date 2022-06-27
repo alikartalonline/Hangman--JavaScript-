c4ce9607e41a0a6c677fa89a0e9a9fc4518a7a8c
Ali Kartal <ali_kartal91@hotmail.com>
2022-06-27 20:00:26 +0300

    update (footer)

--- hangman.js
+++ hangman.js
@@ -5,6 +5,8 @@ const wrongLettersDOM = document.getElementById('wrong-letters');
 const victimDOM = document.querySelectorAll('.victim');
 const letterMessageDOM = document.getElementById('message');
 const playAgainButton = document.getElementById('playAgain');
+const popupAnimation = document.querySelector('.popup');
+const footerDOM = document.querySelector("#footerId")
 
 const correctLetters = [];
 const wrongLetters = [];
@@ -34,6 +36,7 @@ function displayWord() {
         if (realWord == selectedWord) {
             popDOM.style.display = "flex";
             messageDOM.innerText = "Tebrikler, Kazandınız!"
+            popupAnimation.classList.add("popupAnimationSuccess")
         }
 
     }
@@ -60,6 +63,14 @@ function updateWrongLetters() {
     if (wrongLetters.length == victimDOM.length) {
         popDOM.style.display = "flex";
         messageDOM.innerText = "Unfortunately you were hanged :)"
+        popupAnimation.classList.add("popupAnimationFail")
+        wordDOM.innerHTML = `
+    ${selectedWord.split('').map(eachLetter => `
+        <div class="letter text-primary">
+            ${selectedWord.includes(eachLetter) ? eachLetter : ''}
+         </div>
+        `).join('')}
+        `;
     }
 
 }
@@ -100,7 +111,7 @@ window.addEventListener('keydown', e => {
     // console.log(e.keyCode); // 65
     
     // ben "ctrl","alt" gibi tuşların koda eklenmesini istemiyorum, o yüzden 65 ile 90 arasını alacağım.
-    if (e.keyCode >= 65 && e.keyCode <= 90) {
+    if (e.keyCode >= 65 && e.keyCode <= 222) {
         const letter = e.key
 
         // letter yani klavyeden sallayarak oluşturduğumuz harf, selectedWord içerisinde var mı?
@@ -133,7 +144,9 @@ window.addEventListener('keydown', e => {
 
 
 
-
+footerDOM.innerHTML = `${new Date().getFullYear()} Copyright: <a class="alikartalonline" href="https://github.com/alikartalonline"> alikartalonline <svg height="20" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github v-align-middle">
+<path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+</svg></a>`
 
 displayWord();
 
